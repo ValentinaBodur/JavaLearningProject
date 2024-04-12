92baa4dd39b1603014142615f9743c976186b342
ValentinaBodur <bodur.valentina@gmail.com>
2024-04-12 20:09:52 +0300

    I have added a boolean variable

---
+++ src/main/java/md/tekwillacademy/variablesandcomments/CommentsAndVariables.java
@@ -0,0 +1,10 @@
+package md.tekwillacademy.variablesandcomments;
+
+public class CommentsAndVariables {
+    public static void main(String[] args) {
+        boolean isHotOutside;
+        isHotOutside = true;
+        System.out.println("The value of our variable is: "+isHotOutside);
+
+    }
+}
